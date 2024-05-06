6a4b4668b5ac85dd3878326f810a7df7dd8301ed
seozkan <ozkan003@gmail.com>
2024-05-06 22:54:39 +0300

    v1.1

--- ex00/includes/BitcoinExchange.hpp
+++ ex00/includes/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: seozkan < seozkan@student.42kocaeli.com.tr +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 10:15:00 by seozkan           #+#    #+#             */
-/*   Updated: 2024/05/05 12:58:57 by seozkan          ###   ########.fr       */
+/*   Updated: 2024/05/06 22:52:02 by seozkan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,10 @@ public:
     BitcoinExchange(const BitcoinExchange &);
     BitcoinExchange &operator=(const BitcoinExchange &);
     ~BitcoinExchange();
-
+    
     std::map<std::string, double> readData();
     double findRate(std::string date, std::map<std::string, double> data);
     void checkInput(char *file, std::map<std::string, double> data);
-    void printMap(const std::map<std::string, double> &myMap);
     std::string moveDateBackOneDay(const std::string &date);
     std::string intToString(int value);
     bool ifValidDate(const std::string &date);
--- ex00/src/BitcoinExchange.cpp
+++ ex00/src/BitcoinExchange.cpp
@@ -2,8 +2,8 @@
 
 BitcoinExchange::BitcoinExchange() {}
 BitcoinExchange::~BitcoinExchange() {}
-BitcoinExchange::BitcoinExchange(const BitcoinExchange &){}
-BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &){return *this;}
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &) {}
+BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &) { return *this; }
 
 std::map<std::string, double> BitcoinExchange::readData()
 {
@@ -12,9 +12,8 @@ std::map<std::string, double> BitcoinExchange::readData()
     if (!file)
         throw std::runtime_error("Error: Data.csv file not found!");
     std::string line;
-    // Skip first line
     std::getline(file, line);
-    // Read data from file and insert into map
+    float counter = 0;
     while (std::getline(file, line))
     {
         std::stringstream ss(line);
@@ -22,36 +21,39 @@ std::map<std::string, double> BitcoinExchange::readData()
         std::getline(ss, date, ',');
         std::getline(ss, rate, ',');
         csv[date] = std::atof(rate.c_str());
+        counter++;
     }
+    csv["counter"] = counter;
     return csv;
 }
 
-void BitcoinExchange::checkInput(char *file, std::map<std::string, double> data) {
+void BitcoinExchange::checkInput(char *file, std::map<std::string, double> data)
+{
     std::string firstLine;
     std::ifstream inputFile(file);
     std::string line;
 
-    if (!inputFile) {
+    if (!inputFile)
         throw std::runtime_error("Error: Failed to open file.");
-    }
 
     std::getline(inputFile, firstLine);
-    if (firstLine != "date | value") {
+    if (firstLine != "date | value")
         throw std::runtime_error("Error: First line of file is not 'date | value'.");
-    }
 
-    while (std::getline(inputFile, line)) {
+    while (std::getline(inputFile, line))
+    {
         std::istringstream iss(line);
         std::string date;
         std::string value;
         char separator;
-        if (!(iss >> date >> separator >> value) || separator != '|' || !ifValidDate(date) || !ifValidValue(value)) {
+        if (!(iss >> date >> separator >> value) || separator != '|' || !ifValidDate(date) || !ifValidValue(value))
             std::cout << ("Error: bad input => " + date) << std::endl;
-        } else if (strtod(value.c_str(), NULL) < 0) {
+        else if (strtod(value.c_str(), NULL) < 0)
             std::cout << ("Error: not a positive number.") << std::endl;
-        } else if (strtod(value.c_str(), NULL) > 1000) {
+        else if (strtod(value.c_str(), NULL) > 1000)
             std::cout << ("Error: too large a number.") << std::endl;
-        } else {
+        else
+        {
             double mult = strtod(value.c_str(), NULL) * findRate(date, data);
             std::cout << date << " => " << value << " = " << mult << std::endl;
         }
@@ -61,15 +63,23 @@ void BitcoinExchange::checkInput(char *file, std::map<std::string, double> data)
 
 double BitcoinExchange::findRate(std::string date, std::map<std::string, double> data)
 {
-    std::map<std::string, double>::iterator it = data.find(date);
-    if (it != data.end())
-        return it->second;
-    if (date == "not valid date")
-        return 0;
-    return findRate(moveDateBackOneDay(date), data);
+    std::map<std::string, double>::iterator it;
+    float counter = data["counter"];
+
+    while (counter > 0) 
+    {
+        it = data.find(date);
+        if (it != data.end())
+            return it->second;
+        else
+            date = moveDateBackOneDay(date);
+        counter--;
+    }
+    return 0;
 }
 
-std::string BitcoinExchange::intToString(int value) {
+std::string BitcoinExchange::intToString(int value)
+{
     std::ostringstream oss;
     oss << value;
     return oss.str();
@@ -77,19 +87,16 @@ std::string BitcoinExchange::intToString(int value) {
 
 std::string BitcoinExchange::moveDateBackOneDay(const std::string &date)
 {
-    // Gelen string tarih bilgisini parçalara ayırmak için stringstream kullanalım
     std::istringstream ss(date);
     std::string yearStr, monthStr, dayStr;
     std::getline(ss, yearStr, '-');
     std::getline(ss, monthStr, '-');
     std::getline(ss, dayStr, '-');
 
-    // Parçalanan tarih bilgisini integer değerlere dönüştürelim
     int year = std::atoi(yearStr.c_str());
     int month = std::atoi(monthStr.c_str());
     int day = std::atoi(dayStr.c_str());
 
-    // 1 gün öncesini hesaplayalım
     if (day > 1)
         day--;
     else
@@ -103,9 +110,7 @@ std::string BitcoinExchange::moveDateBackOneDay(const std::string &date)
             month--;
         int dayCount;
         if (month == 4 || month == 6 || month == 9 || month == 11)
-        {
             dayCount = 30;
-        }
         else if (month == 2)
         {
             if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
@@ -127,57 +132,34 @@ std::string BitcoinExchange::moveDateBackOneDay(const std::string &date)
 
 bool BitcoinExchange::ifValidDate(const std::string &date)
 {
-    // Check the length of the input string
     if (date.length() != 10)
         return false;
 
-    // Define necessary variables
     int year, month, day;
-    char separator;
+    char sepOne;
+    char sepTwo;
 
-    // Parse the date string
     std::istringstream ss(date);
-    ss >> year >> separator >> month >> separator >> day;
+    ss >> year >> sepOne >> month >> sepTwo >> day;
 
-    // Check for parsing failures and validity of components
-    if (ss.fail() || separator != '-' || year < 0 || month < 1 || month > 12 || day < 1 || day > 31)
+    if (ss.fail() || sepOne != '-' || sepTwo != '-' || year < 0 || month < 1 || month > 12 || day < 1 || day > 31)
         return false;
 
-    // Check for valid day of the month considering leap years
     bool leap_year = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
     if ((month == 2 && (leap_year ? day > 29 : day > 28)) ||
         ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30))
-    {
         return false;
-    }
-
-    // The input string is a valid date
     return true;
 }
 
 bool BitcoinExchange::ifValidValue(const std::string &value)
 {
-    // Check that the input string has the correct length
     std::istringstream iss(value);
     float floatValue;
     iss >> floatValue;
 
     if (iss.fail() || !iss.eof())
-    {
-        // std::cout << "Invalid value" << std::endl;
         return false;
-    }
     else
-    {
         return true;
-    }
-}
-
-
-void BitcoinExchange::printMap(const std::map<std::string, double> &map)
-{
-    std::map<std::string, double>::const_iterator it;
-    for (it = map.begin(); it != map.end(); ++it)
-        std::cout << it->first << "=" << it->second << " ";
-    std::cout << std::endl;
 }
